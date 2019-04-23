ab77f5b46eea0549413bfb4d79fd34a6638cbdc9
naplazz <naplazz@gmail.com>
2019-04-23 16:15:13 +0200

    sdqs

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ var nomi = ['mario', 'luigi', 'gaia'];
 //var newName = prompt('inserisci il tuo nome');
 var i = 0;
 
-while (i < nomi.lenght)
+while (i < nomi.length) {
 
+  document.getElementById("lista-nomi").innerHTML = "<li>" + nomi[i] + "</li>";
+  console.log(nomi[i]);
+  i++;
+}
 
-document.getElementById("lista-nomi").innerHTML = nomi;
-nomi.push(newName);
-nomi.sort();
-document.writeln(nomi);
+// nomi.push(newName);
+// nomi.sort();
+// document.writeln(nomi);
